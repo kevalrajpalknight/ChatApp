c02d35d70a70943dfda87819af2b6b16b6f4494d
kevalrajpalknight <kevalprogram100@gmail.com>
2023-02-22 11:05:15 +0530

    #3 - Added JWT models and modified users table

--- users/models.py
+++ users/models.py
@@ -2,17 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserManager(BaseUserManager):
-    def _create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError("Email field is required")
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError("Username field is required")
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
@@ -24,7 +23,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(unique=True, primary_key=True, max_length=255)
@@ -40,3 +39,11 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+    
+class Jwt(models.Model):
+    user = models.OneToOneField(
+        User, related_name="login_user", on_delete=models.CASCADE)
+    access = models.TextField()
+    refresh = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
